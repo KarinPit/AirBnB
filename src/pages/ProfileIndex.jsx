6b02415e7dc498f18c0e953b2524d5231fb00649
KarinPit <99114944+KarinPit@users.noreply.github.com>
2024-04-01 19:08:13 +0300

    divided the tasks in the stay JSON to the relevant pages/components

--- src/pages/ProfileIndex.jsx
+++ src/pages/ProfileIndex.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+/*
+TO DO:
+// UserDetails
+  //  basic info
+  //  visitedStays => orderService.query({userId: 'u101'})
+  //  myStayOrders => orderService.query({hostId: 'u101'})
+  //  ownedStays => stayService.query({hostId: 'u103'})
+*/
+
 export function ProfileIndex() {
     return (
         <div>
--- src/pages/TravelerIndex.jsx
+++ src/pages/TravelerIndex.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
+
+/*
+TO DO:
+// Homepage: TOP categories: Best Rate / Houses / Kitchen  - show all - link to Explore
+// Renders a <StayList> with <StayPreview> with Link to <StayDetails>   url: /stay/123
+// See More => /explore?topRate=true
+// See More => /explore?type=House
+// See More => /explore?amenities=Kitchen
+// Explore page:
+// stayService.query({type: 'House'})
+*/
+
 export function TravelerIndex() {
     return (
         <div>
