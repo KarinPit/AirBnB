7bc97a07b045f5104b68436bfdac447e005be27e
KarinPit <99114944+KarinPit@users.noreply.github.com>
2024-04-08 15:33:46 +0300

    configured responsive layout for the main page and placed elements inside the layout

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import Logo from "../assets/svg/logo.svg";
 import Language from "../assets/svg/language.svg";
@@ -11,35 +11,41 @@ import "../assets/styles/main.scss"
 export function AppHeader() {
   return (
     <header className="app-header full">
-      <Link to={"/"}>
-        <img className="logo" src={Logo} alt="logo" />
-      </Link>
-      <div className="header__center">
-        <Link to={"/stay"}>
-          <button>Stays</button>
+      <div className="top-row">
+        <Link to={"/"}>
+          <img className="logo" src={Logo} alt="logo" />
         </Link>
-        <Link to={"/experience"}>
-          <button>Experiences</button>
-        </Link>
-        <Link to={"/online-experience"}>
-          <button>Online Experiences</button>
-        </Link>
+
+        <div className="center-row">
+          <Link to={"/stay"}>
+            <button>Stays</button>
+          </Link>
+          <Link to={"/experience"}>
+            <button>Experiences</button>
+          </Link>
+          <Link to={"/online-experience"}>
+            <button>Online Experiences</button>
+          </Link>
+        </div>
+
+        <div className="header-right">
+          <a>
+            <button className="btn-dark">Airbnb your home</button>
+          </a>
+
+          <div>
+            <img src={Language}></img>
+          </div>
+
+          <div>
+            <h5>Account</h5>
+          </div>
+        </div>
       </div>
 
-      <div className="header__right">
-            <a>
-                <button className="btn-dark">Airbnb your home</button>
-            </a>
-            <div>
-                <img src={Language}></img>
-            </div>
-            <div>
-                <h5>Account</h5>
-            </div>
-        </div>
-        <div className="header_search">
-            <FilterStay/>
-        </div>
+      <div className="filter-row">
+        <FilterStay />
+      </div>
     </header>
-  );
+  )
 }
