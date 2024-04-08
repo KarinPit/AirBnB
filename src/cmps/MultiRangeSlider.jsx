1edce3688c97fe461f8776f97e65a67dcab647b3
madn30 <madn30@gmail.com>
2024-04-09 02:33:54 +0300

    remove comments in multiRangeSlider

--- src/cmps/MultiRangeSlider.jsx
+++ src/cmps/MultiRangeSlider.jsx
@@ -8,12 +8,6 @@ export function MultiRangeSlider({ min, max }) {
   const minVal = values.price_min;
   const maxVal = values.price_max;
 
-  // Ensure the min and max values are updated in Formik when the component mounts or the external min/max props change.
-  // useEffect(() => {
-  //   setFieldValue("price_min", min);
-  //   setFieldValue("price_max", max);
-  // }, [min, max, setFieldValue]);
-
   const handleMinChange = (e) => {
     const newValue = Math.min(Number(e.target.value), maxVal - minDistance);
     setFieldValue("price_min", newValue);
