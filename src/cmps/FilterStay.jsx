4d5c1ce2d36acc9efbdd0b1d85ece5e51cade60d
Asaf erdman <asaferdman23@gmail.com>
2024-05-27 20:19:46 +0300

    Fixing the merge conflict on main branch

--- src/cmps/FilterStay.jsx
+++ src/cmps/FilterStay.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setFilterBy, loadStays } from '../store/actions/stay.actions';
 import { useEffectUpdate } from '../customHooks/useEffectUpdate';
 
+
 import DatePicker from 'react-datepicker';
 import GuestPicker from './GuestPicker'; // Import the new component
 
@@ -468,29 +469,12 @@ const handleSubmit = async (e) => {
               </div>
             ))}
           </div>
-
-          <button type="submit" className="search-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 32 32"
-              aria-hidden="true"
-              role="presentation"
-              focusable="false"
-            >
-              <path
-                fill="none"
-                d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9"
-              ></path>
-            </svg>
-          </button>
         </div>
       </div>
     )}
   </div>
 );
-      </form>
-    </div>
-  )
-}
+  
+};
 
 export default FilterStay;
